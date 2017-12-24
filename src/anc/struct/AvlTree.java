4a32562177e8f1424de524b766f61fcf5795a8d3
anchore <anchore@fanruan.com>
2017-12-24 22:40:02 +0800

    avl

--- src/anc/struct/AvlTree.java
+++ src/anc/struct/AvlTree.java
@@ -1,6 +1,8 @@
 package anc.struct;
 
 import java.util.Comparator;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Random;
 import java.util.stream.Stream;
 
@@ -8,264 +10,263 @@ import java.util.stream.Stream;
  * @author anchore
  * @date 2017/11/21
  */
-public class AvlTree<T> {
-    Node<T> treeRoot;
-    Comparator<T> c;
+public class AvlTree<V> {
+    NodeKey treeRoot = new NodeKey();
+    Comparator<V> c;
 
-    /**
-     * handle the always value-reference mechanism of method's parameter in Java
-     */
-    NodeImpl<T> tmp;
+    Map<NodeKey, Node> nodes = new HashMap<>();
 
-    public AvlTree(Comparator<T> c) {
+    public AvlTree(Comparator<V> c) {
         this.c = c;
     }
 
-    void insert(T v) {
-        Node<T> n = new NodeImpl<>(v);
-        if (treeRoot == null) {
-            treeRoot = n;
+    void insert(V v) {
+        if (!has(treeRoot)) {
+            Node n = new Node(treeRoot, v);
+            put(treeRoot, n);
             return;
         }
-        insert(treeRoot, n, c);
+        Node n = new Node(new NodeKey(), v);
+        insert(get(treeRoot), n);
     }
 
     public static void main(String[] args) {
         Random r = new Random();
         AvlTree<Integer> avl = new AvlTree<>(Integer::compareTo);
-        Integer[] integers = Stream.generate(() -> r.nextInt(10)).limit(15).distinct().toArray(Integer[]::new);
+        Integer[] integers = Stream.generate(() -> r.nextInt(15)).distinct().limit(10).toArray(Integer[]::new);
         Stream.of(integers).forEach(avl::insert);
         System.out.println();
     }
 
 
-    private static <T> void insert(Node<T> r, Node<T> n, Comparator<T> c) {
+    private void insert(Node r, Node n) {
         int cmp = c.compare(r.value(), n.value());
         if (cmp > 0) {
-            if (r.left() == null) {
-                r.left(n);
-                r.height(height(r));
+            if (!r.hasLeft()) {
+                r.setLeft(n);
+                r.setHeight(height(r));
                 return;
             }
-            insert(r.left(), n, c);
+            insert(r.left(), n);
             int bf = balanceFactor(r.left());
             if (bf == 2) {
-                r.left(llRotate(r.left()));
+                r.setLeft(llRotate(r.left()));
             } else if (bf == -2) {
-                r.left(lrRotate(r.left()));
+                r.setLeft(lrRotate(r.left()));
             }
         } else {
-            if (r.right() == null) {
-                r.right(n);
-                r.height(height(r));
+            if (!r.hasRight()) {
+                r.setRight(n);
+                r.setHeight(height(r));
                 return;
             }
-            insert(r.right(), n, c);
+            insert(r.right(), n);
             int bf = balanceFactor(r.right());
             if (bf == 2) {
-                r.right(rlRotate(r.right()));
+                r.setRight(rlRotate(r.right()));
             } else if (bf == -2) {
-                r.right(rrRotate(r.right()));
+                r.setRight(rrRotate(r.right()));
             }
         }
 
-        r.height(height(r));
+        r.setHeight(height(r));
 
         int bf = balanceFactor(r);
         if (bf == 2) {
             int lbf = balanceFactor(r.left());
             if (lbf == 1) {
-                r.self(llRotate(r));
+                r.setSelf(llRotate(r));
             } else if (lbf == -1) {
-                r.self(lrRotate(r));
+                r.setSelf(lrRotate(r));
             }
         } else if (bf == -2) {
             int rbf = balanceFactor(r.right());
             if (rbf == 1) {
-                r.self(rlRotate(r));
+                r.setSelf(rlRotate(r));
             } else if (rbf == -1) {
-                r.self(rrRotate(r));
+                r.setSelf(rrRotate(r));
             }
         }
 
-        r.height(height(r));
+        r.setHeight(height(r));
     }
 
     private static <T> void delete() {
 
     }
 
-    private static <T> Node<T> llRotate(Node<T> root) {
+    private Node llRotate(Node root) {
         if (root == null) {
             return null;
         }
-        Node<T> newRoot = root.left();
-        root.left(newRoot.right());
-        newRoot.right(root);
+        Node newRoot = root.left();
+        root.setLeft(newRoot.right());
+        newRoot.setRight(root);
 
-        root.height(height(root));
-        newRoot.height(height(newRoot));
+        root.setHeight(height(root));
+        newRoot.setHeight(height(newRoot));
+
         return newRoot;
     }
 
-    private static <T> Node<T> rrRotate(Node<T> root) {
+    private Node rrRotate(Node root) {
         if (root == null) {
             return null;
         }
-        Node<T> newRoot = root.right();
-        root.right(newRoot.left());
-        newRoot.left(root);
+        Node newRoot = root.right();
+        root.setRight(newRoot.left());
+        newRoot.setRight(root);
 
-        root.height(height(root));
-        newRoot.height(height(newRoot));
+        root.setHeight(height(root));
+        newRoot.setHeight(height(newRoot));
+
         return newRoot;
     }
 
 
-    private static <T> Node<T> lrRotate(Node<T> root) {
+    private Node lrRotate(Node root) {
         if (root == null) {
             return null;
         }
-        Node<T> lc = root.left();
-        Node<T> tmp = lc.right();
-        lc.right(root.right());
-        root.right(tmp);
+        Node lc = root.left();
+        Node tmp = lc.right();
+        lc.setRight(root.right());
+        root.setRight(tmp);
 
-        lc.height(height(lc));
-        root.height(height(root));
+        lc.setHeight(height(lc));
+        root.setHeight(height(root));
+
         return root;
     }
 
-    private static <T> Node<T> rlRotate(Node<T> root) {
+    private Node rlRotate(Node root) {
         if (root == null) {
             return null;
         }
-        Node<T> rc = root.right();
-        Node<T> tmp = rc.left();
-        rc.left(root.left());
-        root.left(tmp);
+        Node rc = root.right();
+        Node tmp = rc.left();
+        rc.setLeft(root.left());
+        root.setLeft(tmp);
 
-        rc.height(height(rc));
-        root.height(height(root));
+        rc.setHeight(height(rc));
+        root.setHeight(height(root));
+
         return root;
     }
 
-    private static <T> int height(Node<T> n) {
+    private int height(Node n) {
         if (n == null) {
             return 0;
         }
-        int lh = n.left() == null ? 0 : n.left().height();
-        int rh = n.right() == null ? 0 : n.right().height();
-        return max(lh, rh) + 1;
+        int lh = n.hasLeft() ? n.left().height() : 0;
+        int rh = n.hasRight() ? n.right().height() : 0;
+        return Math.max(lh, rh) + 1;
     }
 
-    private static <T> int balanceFactor(Node<T> n) {
+    private int balanceFactor(Node n) {
         if (n == null) {
             return 0;
         }
-        int lh = n.left() == null ? 0 : n.left().height();
-        int rh = n.right() == null ? 0 : n.right().height();
+        int lh = n.hasLeft() ? n.left().height() : 0;
+        int rh = n.hasRight() ? n.right().height() : 0;
         return lh - rh;
+    }
+
+    boolean has(NodeKey nodeKey) {
+        return nodes.containsKey(nodeKey);
+    }
+
+    Node get(NodeKey nodeKey) {
+        return nodes.get(nodeKey);
+    }
+
+    void put(NodeKey nodeKey, Node node) {
+        nodes.put(nodeKey, node);
+    }
+
+    class Node {
+        V value;
+        int height = 1;
+        NodeKey self;
+        NodeKey left;
+        NodeKey right;
+
+        V value() {
+            return value;
+        }
+
+        NodeKey key() {
+            return self;
+        }
+
+        void setValue(V value) {
+            this.value = value;
+        }
+
+        int height() {
+            return height;
+        }
+
+        void setHeight(int height) {
+            this.height = height;
+        }
+
+        void setSelf(Node node) {
+            self = node.key();
+        }
+
+        boolean hasLeft() {
+            return has(left) && get(left) != null;
+        }
+
+        Node left() {
+            return get(left);
+        }
+
+        void setLeft(Node node) {
+            left = node.key();
+        }
+
+        boolean hasRight() {
+            return has(right) && get(right) != null;
+        }
+
+        Node right() {
+            return get(right);
+        }
+
+        void setRight(Node node) {
+            right = node.key();
+        }
+
+        Node(NodeKey nodeKey, V value) {
+            this.value = value;
+            this.nodeKey = nodeKey;
+            left = new NodeKey();
+            right = new NodeKey();
+        }
+
+        @Override
+        public String toString() {
+            return "{ v=" + value +
+                    ", h=" + height +
+                    " }";
+        }
     }
 
     private static int max(int a, int b) {
         return a > b ? a : b;
     }
 
-
-    static class NodeImpl<T> implements Node<T> {
-        T v;
-        int h = 1;
-        Node<T> left;
-        Node<T> right;
-
-        Node<T> thiz;
-
-        NodeImpl(T v) {
-            this.v = v;
-            thiz = this;
+    static class NodeKey {
+        @Override
+        public boolean equals(Object obj) {
+            return super.equals(obj);
         }
 
         @Override
-        public Node<T> self() {
-            return thiz;
+        public int hashCode() {
+            return super.hashCode();
         }
-
-        @Override
-        public void self(Node<T> node) {
-            thiz = node.self();
-        }
-
-        @Override
-        public T value() {
-            return self().value();
-        }
-
-        @Override
-        public void value(T t) {
-            self().value(t);
-        }
-
-        @Override
-        public int height() {
-            return self().height();
-        }
-
-        @Override
-        public void height(int h) {
-            self().height(h);
-        }
-
-        @Override
-        public Node<T> left() {
-            return self().left().self();
-        }
-
-        @Override
-        public void left(Node<T> node) {
-            self().left().self(node);
-        }
-
-        @Override
-        public Node<T> right() {
-            return self().right().self();
-        }
-
-        @Override
-        public void right(Node<T> node) {
-            self().right().self(node);
-        }
-
-        @Override
-        public String toString() {
-            return "{ v=" + self().value() +
-                    ", h=" + self().height() +
-                    " }";
-        }
-    }
-
-    interface Node<T> {
-        Node<T> self();
-
-        void self(Node<T> node);
-
-        T value();
-
-        void value(T t);
-
-        int height();
-
-        void height(int h);
-
-        Node<T> left();
-
-        void left(Node<T> node);
-
-        Node<T> right();
-
-        void right(Node<T> node);
-
-        @Override
-        String toString();
     }
 }
